e3a50e8c919094e4d03de5f2566c0547acc1fd1c
klevantrain <klevan@163.com>
2018-03-02 22:31:19 +0800

    update

--- app/controller/auth.js
+++ app/controller/auth.js
@@ -7,16 +7,19 @@ class AuthController extends Controller {
   async auth() {
     console.log(11111111);
     const ctx = this.ctx;
-    // const signature = ctx.request.body.signature;
-    // const timestamp = ctx.request.body.timestamp;
-    // const nonce = ctx.request.body.nonce;
-    // const echostr = ctx.request.body.echostr;
+    const signature = ctx.request.body.signature;
+    const timestamp = ctx.request.body.timestamp;
+    const nonce = ctx.request.body.nonce;
+    const echostr = ctx.request.body.echostr;
 
-    const signature = "ctx.request.body.signature";
-    const timestamp = "ctx.request.body.timestamp";
-    const nonce = "ctx.request.body.nonce";
-    const echostr = "ctx.request.body.echostr";
-
+    // const signature = "ctx.request.body.signature";
+    // const timestamp = "ctx.request.body.timestamp";
+    // const nonce = "ctx.request.body.nonce";
+    // const echostr = "ctx.request.body.echostr";
+    console.log(signature);
+    console.log(timestamp);
+    console.log(nonce);
+    console.log(echostr);
     //1.将token、timestamp和nonce按字母排序排序，并转成字符串拼成一个
     const array = new Array(token,timestamp,nonce);
     array.sort();
