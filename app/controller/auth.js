ee22f89896cab8ffcac0f6f5782cab8493083ddc
klevantrain <klevan@163.com>
2018-03-02 22:34:52 +0800

    update

--- app/controller/auth.js
+++ app/controller/auth.js
@@ -7,11 +7,16 @@ class AuthController extends Controller {
   async auth() {
     console.log(11111111);
     const ctx = this.ctx;
-    const signature = ctx.request.body.signature;
-    const timestamp = ctx.request.body.timestamp;
-    const nonce = ctx.request.body.nonce;
-    const echostr = ctx.request.body.echostr;
+    // const signature = ctx.request.body.signature;
+    // const timestamp = ctx.request.body.timestamp;
+    // const nonce = ctx.request.body.nonce;
+    // const echostr = ctx.request.body.echostr;
+    const signature = ctx.query.signature;
+    const timestamp =ctx.query.timestamp;
+    const nonce =ctx.query.nonce;
+    const echostr = ctx.query.echostr;
 
+    // ctx.query.signature;
     // const signature = "ctx.request.body.signature";
     // const timestamp = "ctx.request.body.timestamp";
     // const nonce = "ctx.request.body.nonce";
