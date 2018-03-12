53d7d821887b471f80efee72dfb3b2e04343b37d
klevantrain <klevan@163.com>
2018-03-13 00:33:28 +0800

    update

--- app/core/base_controller.js
+++ app/core/base_controller.js
@@ -50,39 +50,43 @@ class BaseController extends Controller {
 
 
   async queryApple(key,ctx,type,responseMes,requests){
-    // let url = '';
-    // const appKey = "eb7b94e4ca2da62a";
-    // if(type == "IMEI"){
-    //     url = 'http://api.3023.com/long/apple?app=coverage&key='+ appKey + '&sn=' + key ;
-    // }else if(type == "ID"){
-    //   url = 'http://api.3023.com/long/apple?app=fmi&key='+ appKey + '&sn=' + key ;
-    // }else if(type == "SN_TO_IMEI"){
-    //   url = 'http://api.3023.com/long/apple?app=sntoimei&key='+ appKey + '&sn=' + key ;
-    // }else if(type == "IS_REPAIR"){
-    //   url = 'https://api.3023.com/apple/repair?sn=' + key ;
-    // }
-    // const result = await ctx.curl(url, {
-    //   // data: {key : '6d278cde16510d142a8f7667a4792a28',},
-    //   dataType: 'json',
-    //   headers: {
-    //     'key': '6d278cde16510d142a8f7667a4792a28',
-    //   },
-    // });
-    // // console.log(JSON.stringify(result));
-    // if(result.data.code!=0&&result.data.message!=''){
-    //     responseMes.content = result.data.message + ' ,请输入正确的序列号或者IMEI！';
-    //     return baseSend.sendQueryError(responseMes);
-    //   }else if(result.data.code == 0){
-    //     responseMes.type = type;
-    //     responseMes.key = key;
-    //     responseMes.querys = result.data.data;
-    //
-    //     //修改余额
-    //     ctx.service.auth.updateBlanace(requests,type)
-    //     return baseSend.sendQuerySuccess(responseMes);
-    // }
+    let url = '';
+    const appKey = "eb7b94e4ca2da62a";
+    if(type == "IMEI"){
+        url = 'http://api.3023.com/long/apple?app=coverage&key='+ appKey + '&sn=' + key ;
+    }else if(type == "ID"){
+      url = 'http://api.3023.com/long/apple?app=fmi&key='+ appKey + '&sn=' + key ;
+    }else if(type == "SN_TO_IMEI"){
+      url = 'http://api.3023.com/long/apple?app=sntoimei&key='+ appKey + '&sn=' + key ;
+    }else if(type == "IS_REPAIR"){
+      url = 'https://api.3023.com/apple/repair?sn=' + key ;
+    }
+    const result = await ctx.curl(url, {
+      // data: {key : '6d278cde16510d142a8f7667a4792a28',},
+      dataType: 'json',
+      headers: {
+        'key': '6d278cde16510d142a8f7667a4792a28',
+      },
+    });
+    // console.log(JSON.stringify(result));
+    if(result.data.code!=0&&result.data.message!=''){
+        responseMes.content = result.data.message + ' ,请输入正确的序列号或者IMEI！';
+        return baseSend.sendQueryError(responseMes);
+      }else if(result.data.code == 0){
+        responseMes.type = type;
+        responseMes.key = key;
+        responseMes.querys = result.data.data;
 
-    ctx.service.auth.updateBlanace(requests,type)
+        //修改余额
+        ctx.service.auth.updateBlanace(requests,type)
+        return baseSend.sendQuerySuccess(responseMes);
+    }
+
+    // responseMes.type = type;
+    // responseMes.key = key;
+    // responseMes.querys = result.data.data;
+    // // return baseSend.sendQuerySuccess(responseMes);
+    // return baseSend.sendQuerySuccess(responseMes);
 
 
 
